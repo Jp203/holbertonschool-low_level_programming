87d57c80aba1eafe7f25236e8139364c2fb04df2
Jp203 <jp203@icloud.com>
2019-06-19 21:14:07 -0400

    C program to print text using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *
+ * Print programming is like
+ * building a multilingual puzzle
+ *
+ */
+
+int main()
+{
+	char string[];
+	strcpy(str, "Programming is like building a multilingual puzzle");
+	puts(string);
+	return 0;
+}
