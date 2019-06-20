839643fea9cf433f02039b1b08eec167abdcb236
Jp203 <jp203@icloud.com>
2019-06-19 23:50:18 -0400

    C program that prints text using (printf)

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
 #include <stdio.h>
 
 /**
- *main - Prints "With proper grammar..."
- *return=0
+ *main - Prints "With proper grammar..." to stdout
+ *return=0 
  */
 
-int main()
+int main(void)
 {
 	printf("%s", "with proper grammar,but the outcome is a pieceof art,\n");
 	return (0);
