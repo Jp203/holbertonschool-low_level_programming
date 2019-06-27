49a4c9405e65b0a3b9eead40f064750f38128496
Jp203 <jp203@icloud.com>
2019-06-27 19:10:58 -0400

    Print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *
+ * print_numbers - Print integers 0-9.
+ *
+ */
+
+void print_numbers(void)
+{
+
+	char j;
+	
+	/* Printing numbers starting at 0 up to 9 */
+	for (j = '0'; j <= '9'; i++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
