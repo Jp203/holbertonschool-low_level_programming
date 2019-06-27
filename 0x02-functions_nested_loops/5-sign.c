47b5be9f6f43c4697d9caac7e05f0a31a8d22bb1
Jp203 <jp203@icloud.com>
2019-06-27 12:42:38 -0400

    Determine if a number is positive, negative, or zero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,32 @@
-include "holberton.h"
+#include "holberton.h"
 
 /**
   *
   * print_sign - Prints whether number is positive, negative, or zero
   * @n: the number that will be checked
   * Return:1 when positive, 0 when zero, -1 when negative
-  *
-  */
+  "*/
 
 int print_sign(int n)
 {
+	int j;
+	char p;
+
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+		j = 1;
+	 	p = ('+');
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
-		_putchar('0');
-		return (0);
+		j = 0;
+		p = '0';
 	}
-	else if (n < 0)
+	else (n < 0)
 	{
-		_putchar('-');
-		return (-1);
+		j = -1;
+		p = '-';
 	}
+	_putchar(p);
+	return (j);
 }
