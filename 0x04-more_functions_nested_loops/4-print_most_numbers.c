1c39216f3ee7d14514f3108375f8e2eeec3650a0
Jp203 <jp203@icloud.com>
2019-06-28 01:51:45 -0400

    Print numbers w/ exclusions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *
+ * print_most_numbers - Print numbers 0-9 excluding 2 & 4.
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		if (j != 50 && j !=52)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
+
+
+
