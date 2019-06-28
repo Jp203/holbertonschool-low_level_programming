fe691a22f42b249836566e3d9f9f079d19d198a0
Jp203 <jp203@icloud.com>
2019-06-27 23:36:55 -0400

    Print numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,12 +12,11 @@ void print_numbers(void)
 
 	int j;
 	
-	/* Printing numbers starting at 0 up to 9 */
-	for (j = 0; j <= 9 ; j++)
+	
+	for (j = 57; j >= 48; --j)
 	{
-		/* Using ASCII to increment up from 0 */
-		_putchar(j + 48);
+		
+		_putchar(j);
 	}
 	_putchar('\n');
-	return (0);
 }
