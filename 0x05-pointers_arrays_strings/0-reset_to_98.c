81c8bf931bf05f373756742b8b3741a334e006b6
Jp203 <jp203@icloud.com>
2019-07-02 11:09:28 -0400

    Update integer value that pointer points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * reset_to_98 - Update value of pointer
+  *
+  * @n - pointer to an integer which value will change
+  *
+  * Return: Always 0
+  *
+  */
+void reset_to_98(int *n
+		{
+		*n = 98;
+		}
+
