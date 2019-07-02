e52ec841cb491fdc6f411b1c78133d5b409cb1f2
Jp203 <jp203@icloud.com>
2019-07-02 13:12:26 -0400

    Holberton Header

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,17 +1,40 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef __HOLBERTON__
+#define __HOLBERTON__
 
-int _putchar(char);
-void reset_to_98(int *);
-void swap_int(int *, int *);
-int _strlen(char *);
-void _puts(char *);
-void print_rev(char *);
-void rev_string(char *);
-void puts2(char *);
-void puts_half(char *);
-void print_array(int *, int);
-char *_strcpy(char *, char *);
-int _atoi(char *);
+#include <stdio.h>
+
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_times_table(int n);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int a);
+int print_last_digit(int a);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+int _isupper(int c);
 
 #endif
