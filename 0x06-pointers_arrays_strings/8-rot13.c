634ef442e2a842fd0ddbccbb6868d1a5aa43b566
Jp203 <jp203@icloud.com>
2019-07-09 22:48:24 -0400

    Encode a string into rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -2,32 +2,20 @@
 /**
   * rot13 - Encodes given string by rotating the characters by 13 spaces.
   *
-  * @s: Pointer to string to be encoded.
+  * @s: The string that will be checked.
   *
-  * Return: Encoded string.
+  * Return: Pointer to an encoded string.
   */
 char *rot13(char *s)
 {
 	int j, p;
 
-	char rot[] = "NOQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-	char boo;
+	char a1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (j = 0; s[j] != '\0'; j++)
-	{
-		boo = 0;
-
-		for (j = 0; s[j] != '\0' && boo == 0; p++)
-		{
-			if (s[j] == alphabet[j])
-			{
-				s[j] = rot[j];
-				boo = 1;
-			}
-		}
-	}
+		for (p = 0; p < 52; p++)
+			if (s[j] == a1[p])
+				s[j] = b1[p];
 	return (s);
 }
