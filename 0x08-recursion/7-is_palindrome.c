86415e415d91011f55c8e94268093042bf685937
Jp203 <jp203@icloud.com>
2019-07-10 23:57:41 -0400

    Check for empty string

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * is_palindrome - Checks for empty string.
+  *
+  * @s: String to check.
+  *
+  * Return: 1 if string is empty.
+  */
+
+  int wildcmp(char *s1, char *s2)
+{
+
+}
