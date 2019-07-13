001eb633fa1cd24a8354340070303288ad1cc24f
Jp203 <jp203@icloud.com>
2019-07-12 22:28:13 -0400

    Print programs own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * main - Program will print its own name.
+  *
+  * @argc: The amount of arguments(unused).
+  * @argv: The argument vectors.
+  *
+  * Return: 0 when succesful.
+  */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
