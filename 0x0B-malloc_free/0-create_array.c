824fd734cee4744704862fd2162a1ad9a9ac2b78
Jp203 <jp203@icloud.com>
2019-07-16 12:42:16 -0400

    Create an array, initialized with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,23 +1,30 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  * create_array - Creates a char array & initializes it whith specific char.
+  * create_array - Creates a char array & initializes it with char c.
   * @size: Size of the array.
   * @c: char to initialize the array.
   * Return: Ptr to array on success, NULL on failure.
   */
 char *create_array(unsigned int size, char c)
 {
-	char *a = malloc(size * sizeof(char));
+	char *ptr;
 	unsigned int i;
 
-	/* Check for errors */
-	if (size == 0 || a == NULL)
+	/* Check 1 - Valid input */
+	if (size > 0)
+	{
+		/* Set ptr array to allocated memory */
+		ptr = malloc(size * sizeof(char));
+		/* Check 2 - Size of array*/
+		if (ptr == NULL)
+			return (NULL);
+	}
+	else
 		return (NULL);
-	/* Iterate through array to get length */
+	/* Iterate through size to get length of array */
 	for (i = 0; i < size; i++)
-		/* Set array to char c */
-		a[i] = c;
-	/* Return pointer to the array */
-	return (a);
+		/* Set @c to initialize array */
+		ptr[i] = c;
+	return (ptr);
 }
