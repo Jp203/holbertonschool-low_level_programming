c95fa24c65195d3502aaf9bfd7fbff7ca2a59c9c
Jp203 <jp203@icloud.com>
2019-07-17 13:17:57 -0400

    Allocate memory for an int array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - Allocates memory for an array.
+ * @nmemb: The number of elements in the array.
+ * @size: The size of the elements.
+ * Return: Returns pointer memory of the array. Null if @nmem or @size is 0
+ * or if malloc fails.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int count;
+	char *ptr;
+
+	/* Validity Check */
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	/* Allocate memory */
+	ptr = malloc(nmemb * size);
+	/* NULL Check */
+	if (ptr == NULL)
+		return (NULL);
+	/* Set count to @nmemb value*/
+	for (count = 0; count < nmemb; count++)
+		/* Set memory to zero */
+		ptr[count] = 0;
+	return (ptr);
+}
