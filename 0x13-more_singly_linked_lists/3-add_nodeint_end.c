6033e4294446d805a533701e55da753d0736c550
Jp203 <jp203@icloud.com>
2019-08-06 18:05:54 -0400

    Add new nodes at end of list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,21 +9,26 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *current;
-
+	/* Allocate memory */
 	new = malloc(sizeof(listint_t));
+	/* NULL check */
 	if (new == NULL)
 		return (NULL);
+	/* Inserts new node */
 	new->n = n;
+	/* Directs next of new node to NULL  */
 	new->next = NULL;
-
+	/* NULL check */
 	if (*head == NULL)
 	{
 		*head = new;
 		return (*head);
 	}
 	current = *head;
+	/* Traverses through list until NULL  */
 	while (current->next != NULL)
 		current = current->next;
+	/* Sets new node as last element */
 	current->next = new;
 	return (*head);
 }
