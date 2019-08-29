9e5046174ecac6d126cdcd9a7f20ef26af1384ed
Jp203 <jp203@icloud.com>
2019-08-29 19:21:15 -0400

    fixed header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,6 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add (int, int);
 void print_to_98(int);
-void print_times_table(int);
-	
-	
+void print_times_table(int n);
+void times_table(void);
