103617f8edd0fb9a68960595f2574377c244119c
Jp203 <jp203@icloud.com>
2019-08-29 19:16:39 -0400

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+  * add - add two numbers
+  * @num1: first number
+  * @num2: second number
+  * Return: sum
+  */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = first + second;
+	return (sum);
+}
