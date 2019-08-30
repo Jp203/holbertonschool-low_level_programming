f4f5ef58513bd7958df19abf6cdccaedb62b5ce8
Jp203 <jp203@icloud.com>
2019-08-29 20:02:13 -0400

    Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
-  *
   * print_sign - Prints whether number is positive, negative, or zero
   * @n: the number that will be checked
   * Return:1 when positive, 0 when zero, -1 when negative
-  "*/
+  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	 	_putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
