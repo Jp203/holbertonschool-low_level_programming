ecc368c02732edbf41af5c60a467d91c63e93dc6
Jp203 <jp203@icloud.com>
2019-08-29 20:49:56 -0400

    Print 1st 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,44 +7,20 @@
   */
 int main(void)
 {
-	int count = 2;
-	unsigned long sum, sum1, sum2,num1, num2;
-	unsigned long power, f1, f2, s1, s2;
+	unsigned long count, num1 = 0, num2 = 1, num3;
 
-	num1 = 1;
-	num2 = count;
-	power = 100000000;
-	sum = num1 + num2;
-	printf("%lu, %lu, ", num1, num2);
-	while (sum < power)
+	for (count = 0; count < 50; count++)
 	{
-		sum = num2 + num1;
-		printf("%lu, ", sum);
+		num3 = num1 + num2;
 		num1 = num2;
-		num2 = sum;
-		count++;
-	}
-		f1 = num1 / power;
-		f2 = num1 % power;
-		s1 = num2 / power;
-		s2 = num2 % power;
-	while (count < 98)
-	{
-		sum2 = f2 + s2;
-		sum1 = f1 + s1 + (sum2 / power);
-		sum2 = sum2 % power;
-		if (count == 97)
-			break;
-		if (sum2 <= (power / 10))
-			printf("%lu%08lu, ", sum1, sum2);
+		num2 = num3;
+		printf("%lu", num3);
+		if (count == 49)
+			putchar('\n');
 		else
-			printf("%lu%lu, ", sum1, sum2);
-		f1 = s2;
-		f2 = s2;
-		s2 = sum1;
-		s2 = sum2;
-		count++;
+		{
+			printf(", ");
+		}
 	}
-	printf("%lu%lu\n", sum1, sum2);
 	return (0);
 }
