529e99a5fd13e7e18f2a23c2cad2b80ffa1e309d
Jp203 <jp203@icloud.com>
2019-08-29 22:31:52 -0400

    Convert a string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+/**
+  * _atoi - convert a string to an integer
+  * @s: string to convert
+  * Return: integer converted, 0 if string empty of numbers
+  */
+int _atoi(char *s)
+{
+	unsigned int result;
+	int line;
+
+	line = 1;
+	while ((*s < '0' || *s > '9') && *s != '\0')
+	{
+		if (*s == '-')
+			line *= -1;
+		s++;
+	}
+
+	for (result = 0; *s != '\0' && *s >= '0' && *s <= '9'; s++)
+	{
+		result = result * 10 + (*s - '0');
+	}
+	return (value * line);
+}
