c65df5943327482df73a2a06bf20a785fee18df2
Jp203 <jp203@icloud.com>
2019-08-29 20:43:35 -0400

    Print 1st 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,31 +1,50 @@
-#include "holberton.h"
 #include <stdio.h>
+
 /**
- * fibonacci - it shows Fibonacci Series.
- */
+  * main - print first 98 fibonacci numbers
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	int num = 2;
-	double i = 1;
-	double result = 2;
-	
-	while (num <= 50)
+	int count = 2;
+	unsigned long sum, sum1, sum2,num1, num2;
+	unsigned long power, f1, f2, s1, s2;
+
+	num1 = 1;
+	num2 = count;
+	power = 100000000;
+	sum = num1 + num2;
+	printf("%lu, %lu, ", num1, num2);
+	while (sum < power)
 	{
-		if (num == 2)
-			printf("1, 2, ");
-		else
-		{
-			result = result + i;
-			printf("%g", result);
-			i = result - i;
-			if (num == 50)
-				printf("\n");
-			else
-			{
-				printf(", ");
-			}
-		}
-		num++;
+		sum = num2 + num1;
+		printf("%lu, ", sum);
+		num1 = num2;
+		num2 = sum;
+		count++;
 	}
+		f1 = num1 / power;
+		f2 = num1 % power;
+		s1 = num2 / power;
+		s2 = num2 % power;
+	while (count < 98)
+	{
+		sum2 = f2 + s2;
+		sum1 = f1 + s1 + (sum2 / power);
+		sum2 = sum2 % power;
+		if (count == 97)
+			break;
+		if (sum2 <= (power / 10))
+			printf("%lu%08lu, ", sum1, sum2);
+		else
+			printf("%lu%lu, ", sum1, sum2);
+		f1 = s2;
+		f2 = s2;
+		s2 = sum1;
+		s2 = sum2;
+		count++;
+	}
+	printf("%lu%lu\n", sum1, sum2);
 	return (0);
 }
