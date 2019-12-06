da7bec744d451eb50d6deec6d13047910b2a544e
Jp203 <jp203@icloud.com>
2019-12-06 12:57:58 -0500

    fixed

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        size_t total_leaves = 0, l_side =0, r_side = 0;
+        size_t total_leaves = 0, l_side = 0, r_side = 0;
 
         if (!tree)
                 return (0);
