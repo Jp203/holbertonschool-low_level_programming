33c01f6822137adb9b731e3cbffe56f75cf950d7
Jp203 <jp203@icloud.com>
2019-12-06 12:38:26 -0500

    fixed if condition

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
-
+#include <stdlib.h>
 /**
  * binary_tree_delete - delete an entire binary tree
  * @tree: pointer to root node of tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-        return;
-
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    free(tree);
+        if (!tree)
+                return;
+        if (tree->left != NULL)
+                binary_tree_delete(tree->left);
+        if (tree->right != NULL)
+                binary_tree_delete(tree->right);
+        free(tree);
 }
