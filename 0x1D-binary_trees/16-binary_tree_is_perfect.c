3719d0b29c9c5e494dd0e72c7cb3eb8f1ecae8a9
Jp203 <jp203@icloud.com>
2019-12-20 13:58:59 -0500

    fix

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -45,7 +45,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     if (!tree)
         return (0);
 
-    if (tree_depth(tree, depth, 0))
+    if (test_perfection(tree, tree_depth, 0))
         return (1);
     return (0);
 }
